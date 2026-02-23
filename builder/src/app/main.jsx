13447dc3df3a290f5d1756fea2653af2eb4145aa
TomCat2357 <gk3t.mtmr@gmail.com>
2026-02-23 14:44:12 +0900

    テーマについて各テーマについて差分だけ設定できるように変更

--- builder/src/app/main.jsx
+++ builder/src/app/main.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./theme/theme.css";
-import "./theme/themes/standard.css";
-import "./theme/themes/matcha.css";
-import "./theme/themes/sakura.css";
-import "./theme/themes/warm.css";
-import "./theme/themes/ocean.css";
 import "./theme/base.css";
 import "./theme/preview-overrides.css";
+
+// themesフォルダ内のすべてのCSSを自動的に読み込む
+import.meta.glob('./theme/themes/*.css', { eager: true });
 import { DEFAULT_THEME, initTheme } from "./theme/theme.js";
 
 initTheme(DEFAULT_THEME);
